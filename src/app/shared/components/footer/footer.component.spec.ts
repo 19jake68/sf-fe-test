109f634e3f87b81aad3d7846f77ff29825b4e8b4
Jake Ortega <jacob.ortega@dxc.com>
2022-11-14 22:45:04 +0800

    [WIP] Unit Testing

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer.component';
 
@@ -8,7 +9,10 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent ],
+      imports: [
+        RouterModule.forRoot([]),
+      ]
     })
     .compileComponents();
   });
